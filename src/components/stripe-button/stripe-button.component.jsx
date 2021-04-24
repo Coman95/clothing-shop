ee4ba85d4cb04948aa25b5bffa55890192fd3e11
Cosmin Coman <comancosmin95@gmail.com>
2021-04-24 19:57:58 +0300

    updating stripe integrationg and upodating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_Yicrg6r2PdcNKdJdhQ4SZh4N00wXfrKhwM';
+
+	const onToken = (token) => {
+		console.log(token);
+		alert('Payment successful');
+	};
+
+	return (
+		<StripeCheckout
+			label='Pay Now'
+			name='CRWN Clothing Ltd.'
+			billingAddress
+			shippingAddress
+			image='https://sendeyo.com/en/f3eb2117da'
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel='Pay Now'
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
