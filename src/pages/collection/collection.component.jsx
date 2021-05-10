9dbc9892b0c1ad5ba42175835d829eade8ec4ab7
Cosmin Coman <comancosmin95@gmail.com>
2021-05-10 17:56:53 +0300

    replace local data with firestore data and store it into the shop reducer, also included WithSpinner HOC

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
-	console.log(collection);
+	// console.log(collection);
 	const { title, items } = collection;
 	return (
 		<div className='collection-page'>
